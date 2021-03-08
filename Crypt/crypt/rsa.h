0f22ae667d6fdb6fe7720e8dbcf3b38fab8bf45a
FORESTIER RYAN p1714862 <ryan.forestier@etu.univ-lyon1.fr>
2021-03-08 09:57:37 +0100

    Ajout des fonctions

--- Crypt/crypt/rsa.h
+++ Crypt/crypt/rsa.h
@@ -43,6 +43,13 @@ public:
 	virtual bigint encode(const bigint& source, Key* key, unsigned int padsize = PADSIZE);
 	virtual bigint decode(const bigint& source, Key* key, unsigned int padsize = PADSIZE);
 
+	virtual bigint random_integer();
+	virtual bool prime(const bigint& num);
+	virtual bigint euclide(const bigint& a, bigint& b);
+	virtual bigint modpow(const bigint& base, bigint& e, bigint& m);
+	virtual bigint toBinary(const bigint& m);
+	virtual bigint exposant_code(const bigint& m);
+	
 	virtual RsaKey* generate();
 
 	virtual bigint run(const bigint& source, Key* key);
